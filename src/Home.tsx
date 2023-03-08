f19cbb6a29623aec5a3a34f0c663c4da78b7f619
TalMoshel1 <89250740+TalMoshel1@users.noreply.github.com>
2023-03-08 15:08:23 +0200

    update

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,28 +1,24 @@
-import GameProvider from "./store/gameContext";
+import GameProvider from "./store/reducer";
 import { Outlet } from "react-router-dom";
 import styled from "styled-components";
 import { ThemeProvider } from "styled-components";
-import { themeLight, themeDark } from "./theme/theme";
 import {useState} from 'react'
 function Home({ className }: { className?: string }) {
-  const [theme, useTheme] = useState(themeLight)
     return (
     <div className={className}>
-      <ThemeProvider theme={theme}>
         <GameProvider>
           <Outlet></Outlet>
         </GameProvider>
-      </ThemeProvider>
     </div>
   );
 }
 
 export default styled(Home)`
-  background-color: #ffc700;
-  width: 100%;
-  overflow: hidden;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+//   background-color: #ffc700;
+//   width: 100%;
+//   overflow: hidden;
+//   height: 100vh;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
 `;
---
+++ src/components/Graph.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import {Doughnut} from 'react-chartjs-2'
+import {Chart, ArcElement} from 'chart.js'
+import { Labels } from './Labels.js'
+{/* @ts-ignore */}
+import {chart_Data, getTotal} from '../helper/helper'
+{/* @ts-ignore */}
+import {default as api} from '../store/apiSlice'
+
+Chart.register(ArcElement)
+
+  
+export function Graph() {
+
+    const {data, isFetching, isSuccess, isError} = api.useGetLabelsQuery()
+    let graphData;
+
+
+    if (isFetching) {
+        graphData = <div>Fetching</div>
+    } else if (isSuccess) {
+        graphData = <Doughnut {...chart_Data(data)
+        }></Doughnut>
+
+    } else if (isError) {
+        graphData = <div>Error</div>
+    }
+    // api.useGetCategoriesQuery() // the method 'getCategories' converted to useGetCategoriesQuery by redux toolkit query' 
+
+
+    chart_Data()
+    return (
+        <div className='flex justify-content max-w-xs mx-auto'>
+            <div className='item'>
+                <div className='chart relative'>
+                    {graphData}
+                    <h3 className='mb-4 font-bold title'>Total</h3>
+                    <span className='block text-3xl text-emerald-400'>${getTotal(data) ?? 0}</span>
+                </div>
+
+                <div className="flex flex-col py-10 gap-4">
+                    <Labels/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
